0a1a2059e1d28519982203527c32cf7ce9f57b53
lucparedes <lucianacparedes34@gmail.com>
2022-03-13 22:54:14 -0300

    edita footer.jsx

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -3,8 +3,11 @@ import {
   Container,
   Row,
   Col,
-  ListGroup  
+  ListGroup,
+  Card,
 } from "react-bootstrap";
+import { alignPropType } from "react-bootstrap/esm/types";
+
 
 
 /* import Container from "react-bootstrap" */
@@ -13,11 +16,18 @@ const Footer = () => {
 return (
 	<Container>
     <Row>
-      <h2>Footer MK1</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, numquam excepturi. Cum sint, voluptas autem impedit doloribus facilis? Voluptatum itaque blanditiis explicabo a aut. Aliquam dolore earum doloribus non dolorum optio cupiditate. Eos ex sit dicta quasi tempora voluptas doloremque illum consequuntur porro beatae odio, ab impedit, fugiat eaque minus adipisci!</p>
-      
+      <h2>Sobre Nosotros</h2>
+        <blockquote className="blockquote mb-0">
+      <p>{' '}Si quieres quitar el hambre a un hombre dale un pez. Pero si quieres que no vuelva a tener hambre, enséñale a Pescar.{' '}</p>
+      <footer className="blockquote-footer">Fundación Pescar / <cite title="Source Title">Lao Tsé</cite></footer>
+        </blockquote>
+
+<p>Somos compañeros de la fundación Pescar, creamos esta página web con motivo de realizar la primera cohorte</p>
+ 
     </Row>
-    <Row>
+
+{/* 
+
       <Col>Acerca de Nosotros
       <ul>
         <li href="#">1</li>
@@ -25,31 +35,41 @@ return (
         <li>3</li>
       </ul>
       </Col>
-      <Col>Servicios
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-      </Col>
-      <Col>Contactenos
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-      </Col>
-      <Col>Redes Sociales
+*/}
+
+<Row>
+      
+<Col>Equipo de Desarrolladores 
         <ListGroup defaultActiveKey="#link1">
           <ListGroup.Item action href="#link1">
-          Link 1
+          Lucas GV
           </ListGroup.Item>
           <ListGroup.Item action href="#link2">
-          Link 2
+          Gonzalo V
+          </ListGroup.Item>
+          <ListGroup.Item action href="#link3">
+          Áxel R
+          </ListGroup.Item>
+          <ListGroup.Item action href="#link4">
+          Luciana CP
           </ListGroup.Item>
         </ListGroup>
+        </Col>
+    
+      <Col>Tecnologías utilizadas
+      <ul>
+        <li>HTML, CSS</li>
+        <li>JAVASCRIPT, NODE.JS</li>
+        <li>BOOSTRACK</li>
+      </ul>
       </Col>
-    </Row>
+      
+        </Row>
+
+    
+
+ 
+
 	</Container>
 );
 };
