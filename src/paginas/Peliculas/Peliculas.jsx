e4d9e3820940b38a676f4e249758970643639914
gonzaloev <gonzaloevalino@gmail.com>
2022-03-18 00:10:59 -0300

    Paginas linkeadas

--- src/paginas/Peliculas/Peliculas.jsx
+++ src/paginas/Peliculas/Peliculas.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import NavigationBar from "../../componentes/navBar/navigationBar";
 
 function Peliculas() {
   return (
-    <div style={{backgroundColor:'red'}}>Peliculas</div>
-  )
+    <div style={{ backgroundColor: "red" }}>
+      <NavigationBar/>
+      <h1 style={{ marginTop: "100px" }}>Peliculas</h1>
+      <p>Laboris sunt mollit tempor do excepteur proident ea aliquip culpa nostrud aute nostrud aliqua minim. Incididunt sunt pariatur labore incididunt eiusmod aute culpa. Ipsum ipsum elit laborum non culpa consequat dolore. Nisi ad dolore amet exercitation. Ut deserunt labore incididunt ipsum laborum anim velit dolore tempor esse aliquip consectetur. Eiusmod qui nisi ut amet cillum id cupidatat eiusmod ullamco id nulla. Enim sit veniam ut pariatur sit ex nostrud esse nisi do fugiat culpa non.</p>
+    </div>
+  );
 }
 
-export default Peliculas
+export default Peliculas;
