14a28475f5e45d2cc6df5c75d21ed1ffffc2f9c5
gonzaloev <gonzaloevalino@gmail.com>
2022-03-21 08:51:23 -0300

    Renew Key

--- src/paginas/testApi.jsx
+++ src/paginas/testApi.jsx
@@ -17,7 +17,7 @@ function TestApiList() {
             "method": "GET",
             "headers": {
                 "x-rapidapi-host": "movies-app1.p.rapidapi.com",
-                "x-rapidapi-key": "32d6f1946emshd01b80456f42b38p1566bdjsn6a1d1e0303cc" // aca pasamos la key, si no no funciona la api
+                "x-rapidapi-key": 'a739b44996msh9d4a4edbc190029p126fa7jsnb4d18d164d32' // aca pasamos la key, si no no funciona la api
             }
         })
             .then((res) => res.json()) // devolvemos el JSON
